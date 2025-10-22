c388fa3f1323e29f0a6c0468bca64b5800aa56d0
Kyudan Jung (정규단) <116885375+KyuDan1@users.noreply.github.com>
2025-10-22 16:27:09 +0900

    first commit

---
+++ benchmark/main_evaluation.py
@@ -0,0 +1,21 @@
+## TO DO
+## dataset download
+
+
+## TODO
+## model download or api load
+
+
+## TODO
+## form input
+
+
+## TODO
+## input model and get output
+
+
+## TODO
+## benchmark evaluation
+
+## TODO
+## print..
